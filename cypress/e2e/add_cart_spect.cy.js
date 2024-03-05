70e31ec4a568b39c378d9f4e5766c98d16c63c55
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-05 11:53:37 +0800

    feat : add test case for filter

--- cypress/e2e/add_cart_spect.cy.js
+++ cypress/e2e/add_cart_spect.cy.js
@@ -1,25 +1,20 @@
 describe('add cart feature', () => {
     beforeEach(() => {
         cy.visit('https://www.saucedemo.com')
-    })
 
-    it('should add item to cart', () => {
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
     
         cy.url().should('include', '/inventory.html')
-    
+    })
+
+    it('should add item to cart', () => {
         cy.get('#add-to-cart-sauce-labs-backpack').click()
         cy.get('.shopping_cart_badge').should('have.text', '1')
     })
     
     it('should add multiple items to cart', () => {
-        cy.get('#user-name').type('standard_user')
-        cy.get('#password').type('secret_sauce')
-        cy.get('#login-button').click()
-    
-        cy.url().should('include', '/inventory.html')
     
         cy.get('#add-to-cart-sauce-labs-backpack').click()
         cy.get('#add-to-cart-sauce-labs-bike-light').click()
@@ -27,4 +22,25 @@ describe('add cart feature', () => {
     
         cy.get('.shopping_cart_badge').should('have.text', '3')
     })
+
+    it('should sort catalogue from name A to Z', () => {
+        cy.get('.product_sort_container').select('Name (A to Z)')
+        cy.get('.inventory_item').first().get('.inventory_item_name ').first().should('have.text', 'Sauce Labs Backpack')
+    })
+
+    it('should sort catalogue from name Z to A', () => {
+        cy.get('.product_sort_container').select('Name (Z to A)')
+        cy.get('.inventory_item').first().get('.inventory_item_name ').first().should('have.text', 'Test.allTheThings() T-Shirt (Red)')
+    })
+
+    it('should sort catalogue from Price (low to high)', () => {
+        cy.get('.product_sort_container').select('Price (low to high)')
+        cy.get('.inventory_item').first().get('.inventory_item_name ').first().should('have.text', 'Sauce Labs Onesie')
+    })
+
+    it('should sort catalogue from Price (high to low)', () => {
+        cy.get('.product_sort_container').select('Price (high to low)')
+        cy.get('.inventory_item').first().get('.inventory_item_name ').first().should('have.text', 'Sauce Labs Fleece Jacket')
+    })
+
 });
